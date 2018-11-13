cbf0a97baa338224c2bb92e801fc5213c6396622
bartkoevoets <yzig_koevoets@hotmail.com>
2018-11-13 13:17:55 +0100

    Temp commit for checkout

---
+++ FSBeheer/FSBeheer/ViewModel/EventManagementViewModel.cs
@@ -0,0 +1,23 @@
+﻿using FSBeheer.VM;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSBeheer.ViewModel
+{
+    public class EventManagementViewModel : ViewModelBase
+    {
+        private CustomFSContext _Context;
+
+        public ObservableCollection<EventVM> Events { get; set; }
+        public EventVM SelectedEvent { get; set; }
+
+        public RelayCommand EditCreateEventCommand { get; set; }
+        public RelayCommand 
+    }
+}
