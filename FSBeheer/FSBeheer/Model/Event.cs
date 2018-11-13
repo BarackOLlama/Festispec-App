761a20f108e7b2f40029b0e1b7ce24915b1841af
bartkoevoets <yzig_koevoets@hotmail.com>
2018-11-13 12:08:31 +0100

    Added Event model

---
+++ FSBeheer/FSBeheer/Model/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSBeheer.Model
+{
+    public partial class Event
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public int? CustomerId { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
