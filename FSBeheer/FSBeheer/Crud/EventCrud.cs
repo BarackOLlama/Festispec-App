dfeaa9e66d805a1091a4980bce1ac0437cac9f23
Evert <evert-arends@hotmail.nl>
2018-11-14 15:12:14 +0100

    Added filter option Events

--- FSBeheer/FSBeheer/Crud/EventCrud.cs
+++ FSBeheer/FSBeheer/Crud/EventCrud.cs
@@ -1,17 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FSBeheer.VM;
 
 namespace FSBeheer.Crud
 {
-    class EventCrud: AbstractCrud
+    class EventCrud : AbstractCrud
     {
         public EventCrud(CustomFSContext customFSContext) : base(customFSContext)
         {
 
         }
-        
+        public ObservableCollection<EventVM> GetAllEventVMs()
+        {
+            var events = CustomFSContext.Events
+               .ToList()
+               .Select(c => new EventVM(c));
+            var _events = new ObservableCollection<EventVM>(events);
+
+            return _events;
+        }
+
+        public ObservableCollection<EventVM> GetFilteredEventsByString(string must_contain)
+        {
+            if (must_contain == null)
+            {
+                throw new ArgumentNullException(nameof(must_contain));
+            }
+            var events = CustomFSContext.Events
+                .ToList()
+                .Where(c =>
+                c.Name.Contains(must_contain) ||
+                c.City.Contains(must_contain) ||
+                c.Customer.Name.Contains(must_contain) ||
+                c.Address.Contains(must_contain) ||
+                c.Id.Equals(must_contain)
+                )
+
+                .Select(c => new EventVM(c));
+            var _events = new ObservableCollection<EventVM>(events);
+
+            return _events;
+
+        }
     }
 }
