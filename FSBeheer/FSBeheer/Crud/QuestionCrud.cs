288a0ae52188ab3d6892b0eb1f50437eec166b86
Evert <evert-arends@hotmail.nl>
2018-11-14 15:23:26 +0100

    Added QuestionCrud

---
+++ FSBeheer/FSBeheer/Crud/QuestionCrud.cs
@@ -0,0 +1,29 @@
+﻿using FSBeheer.VM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace FSBeheer.Crud
+{
+    class QuestionCrud : AbstractCrud
+    {
+        public QuestionCrud(CustomFSContext customFSContext) : base(customFSContext)
+        {
+
+        }
+        public void Add(QuestionVM _question) => CustomFSContext.Questions.Add(_question.ToModel());
+
+        public ObservableCollection<QuestionVM> GetAllQuestionVMs()
+        {
+            var questions = CustomFSContext.Questions
+               .ToList()
+               .Select(c => new QuestionVM(c));
+            var result = new ObservableCollection<QuestionVM>(questions);
+
+            return result;
+        }
+    }
+}
