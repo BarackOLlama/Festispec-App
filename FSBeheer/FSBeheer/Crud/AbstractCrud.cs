ab6ac90e1baf155734bae38808894b631f56c6ec
Evert <evert-arends@hotmail.nl>
2018-11-14 14:44:58 +0100

    Added EventCrud Added abstract class for Crud actions

---
+++ FSBeheer/FSBeheer/Crud/AbstractCrud.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSBeheer.Crud
+{
+    class AbstractCrud
+    {
+        protected CustomFSContext CustomFSContext;
+
+        public AbstractCrud(CustomFSContext customFSContext)
+        {
+            CustomFSContext = customFSContext;
+        }
+    }
+}
