6f1df5fa37fdedd5a8a5b3a62ba887e9276c64fd
Yekcimickey <31481455+Yekcimickey@users.noreply.github.com>
2018-11-16 12:56:05 +0100

    fixes voor pull request

--- FSBeheer/FSBeheer/Crud/InspectionCrud.cs
+++ FSBeheer/FSBeheer/Crud/InspectionCrud.cs
@@ -8,73 +8,23 @@ using System.Threading.Tasks;
 
 namespace FSBeheer.Crud
 {
-    class InspectionCrud
+    class InspectionCrud : AbstractCrud
     {
 
-        private CustomFSContext _customFSContext;
-
-        public InspectionCrud(CustomFSContext customFSContext)
+        public InspectionCrud(CustomFSContext customFSContext) : base(customFSContext)
         {
-            _customFSContext = customFSContext;
         }
 
         public ObservableCollection<InspectionVM> GetInspectionVMs => _getInspections();
 
         private ObservableCollection<InspectionVM> _getInspections()
         {
-            var _inspection = _customFSContext.Inspections
+            var _inspection = CustomFSContext.Inspections
                 .ToList()
                 .Select(i => new InspectionVM(i));
             var _inspections = new ObservableCollection<InspectionVM>(_inspection);
 
             return _inspections;
         }
-
-        //private ObservableCollection<CustomerVM> _getMultipleInspectionsByName(string name_contains)
-        //{
-        //    if (name_contains == null)
-        //    {
-        //        throw new ArgumentNullException(nameof(name_contains));
-        //    }
-
-        //    var customer = CustomFSContext.Inspections
-        //       .ToList()
-        //       .Where(c => c.Name.Contains(name_contains))
-        //       .Select(c => new CustomerVM(c));
-        //    var _customers = new ObservableCollection<CustomerVM>(customer);
-
-        //    return _customers;
-        //}
-
-        ///*
-        // * Returns one customer based on ID
-        // */
-        //public ObservableCollection<CustomerVM> GetCustomerById(int customer_id)
-        //{
-        //    var customer = CustomFSContext.Customers
-        //       .ToList()
-        //       .Where(c => c.Id == customer_id)
-        //       .Select(c => new CustomerVM(c));
-        //    var _customers = new ObservableCollection<CustomerVM>(customer);
-
-        //    return _customers;
-        //}
-
-        //public void Add(CustomerVM _customer) => CustomFSContext.Customers.Add(_customer.ToModel());
-
-        //public void Modify(CustomerVM _customer)
-        //{
-        //    // SelectedCustomer
-        //    CustomFSContext.Entry(_customer?.ToModel()).State = EntityState.Modified;
-        //    CustomFSContext.SaveChanges();
-        //}
-
-        //public void Delete(CustomerVM _customer)
-        //{
-        //    CustomFSContext.Customers.Attach(_customer?.ToModel());
-        //    CustomFSContext.Customers.Remove(_customer?.ToModel());
-        //    CustomFSContext.SaveChanges();
-        //}
-
     }
 }
--- FSBeheer/FSBeheer/Crud/InspectorCrud.cs
+++ FSBeheer/FSBeheer/Crud/InspectorCrud.cs
@@ -8,20 +8,18 @@ using System.Threading.Tasks;
 
 namespace FSBeheer.Crud
 {
-    class InspectorCrud
+    class InspectorCrud : AbstractCrud
     {
 
-        private CustomFSContext _customFSContext;
+        public InspectorCrud(CustomFSContext customFSContext) : base(customFSContext)
+        {
+        }
 
         public ObservableCollection<CustomerVM> GetCustomerVMs => _getCustomers();
-        public InspectorCrud(CustomFSContext customFSContext)
-        {
-            _customFSContext = customFSContext;
-        }
 
         private ObservableCollection<CustomerVM> _getCustomers()
         {
-            var inspector = _customFSContext.Customers
+            var inspector = CustomFSContext.Customers
                .ToList()
                .Select(c => new CustomerVM(c));
             var _customers = new ObservableCollection<CustomerVM>(inspector);
