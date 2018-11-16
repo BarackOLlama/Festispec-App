2b8b44d86380f552b9039fafa57792ec649a837f
CJ Lancaster <lancaster.mmxv@outlook.com>
2018-11-16 11:51:01 +0100

    inspection vm/crud in progress.

---
+++ FSBeheer/FSBeheer/Crud/InspectorCrud.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSBeheer.Crud
+{
+    class InspectorCrud
+    {
+        //private CustomFSContext _customFSContext;
+
+        //public ObservableCollection<InspectorVM> GetInspectorVMs => _getInspector();
+        //public InspectorCrud(CustomFSContext customFSContext)
+        //{
+        //    _customFSContext = customFSContext;
+        //}
+
+        //private ObservableCollection<CustomerVM> _getCustomers()
+        //{
+        //    var customer = _customFSContext.Customers
+        //       .ToList()
+        //       .Select(c => new InspectorVM(c));
+        //    var _customers = new ObservableCollection<InspectorVM>(inspector);
+
+        //    return _customers;
+        //}
+    }
+}
--- FSBeheer/FSBeheer/VM/InspectorVM.cs
+++ FSBeheer/FSBeheer/VM/InspectorVM.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FSBeheer.Crud
+namespace FSBeheer.VM
 {
-    class InspectionCrud
+    public class InspectorVM
     {
+        public InspectorVM()
+        {
+
+        }
     }
 }
