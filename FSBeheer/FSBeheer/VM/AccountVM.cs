fbf5ea2581cfb18e4ec57234d783ab6e9cc04bcf
gharamorth <mappelma@avans.nl>
2018-11-22 12:06:13 +0100

    Added components to homeviewmodel relating to login and vragenlijst -commands, stringformats, etc -renaming class(es) -rebinding commands

--- FSBeheer/FSBeheer/VM/AccountVM.cs
+++ FSBeheer/FSBeheer/VM/AccountVM.cs
@@ -16,6 +16,11 @@ namespace FSBeheer.VM
             _account = account;
         }
 
+        public AccountVM()
+        {
+            _account = new Account();
+        }
+
         public int Id
         {
             get { return _account.Id; }
@@ -24,11 +29,13 @@ namespace FSBeheer.VM
         public string Username
         {
             get { return _account.Username; }
+            set { _account.Username = value; }
         }
 
         public string Password
         {
             get { return _account.Password; }
+            set { _account.Password = value; }
         }
 
         public string Salt
