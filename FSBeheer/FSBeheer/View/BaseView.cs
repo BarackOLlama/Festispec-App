c897d4d2d53a7c616ce93971bf4cf8c3eca05516
bartkoevoets <yzig_koevoets@hotmail.com>
2018-11-24 22:56:11 +0100

    Created baseview and building code-behind resources

---
+++ FSBeheer/FSBeheer/View/BaseView.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace FSBeheer.View
+{
+    public partial class BaseView : Window
+    {
+        public BaseView()
+        {
+            var InterfaceButtonFactory = new FrameworkElementFactory(typeof(Button));
+            InterfaceButtonFactory.SetValue(ContentProperty, new TemplateBindingExtension(ContentProperty));
+            InterfaceButtonFactory.SetValue(Button.CommandProperty, new TemplateBindingExtension(Button.CommandProperty));
+            InterfaceButtonFactory.SetValue(Button.CommandParameterProperty, new TemplateBindingExtension(Button.CommandParameterProperty));
+            InterfaceButtonFactory.SetValue(FontSizeProperty, (double)15);
+            InterfaceButtonFactory.SetValue(MarginProperty, new Thickness(5));
+
+            var SearchTextBoxFactory = new FrameworkElementFactory(typeof(TextBox));
+            SearchTextBoxFactory.SetValue(FontSizeProperty, (double)15);
+            SearchTextBoxFactory.SetValue(WidthProperty, (double)300);
+            SearchTextBoxFactory.SetValue(TextBox.TextProperty, new TemplateBindingExtension(TextBox.TextProperty));
+
+            var SearchButtonFactory = new FrameworkElementFactory(typeof(Button));
+            SearchButtonFactory.SetValue(ContentProperty, new TemplateBindingExtension(ContentProperty));
+
+            this.Resources = new ResourceDictionary
+            {
+                {
+                    "InterfaceButton",
+                    new ControlTemplate{ VisualTree = InterfaceButtonFactory }
+                }
+            };
+        }
+    }
+}
