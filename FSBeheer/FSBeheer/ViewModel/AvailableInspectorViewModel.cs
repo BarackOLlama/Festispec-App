5e3fb651e997afce2d754e3a4baa68b78c15a385
Phi <vpnguyen@avans.nl>
2018-11-28 13:59:05 +0100

    commands added

--- FSBeheer/FSBeheer/ViewModel/AvailableInspectorViewModel.cs
+++ FSBeheer/FSBeheer/ViewModel/AvailableInspectorViewModel.cs
@@ -1,12 +1,14 @@
 ﻿using FSBeheer.VM;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace FSBeheer.ViewModel
 {
@@ -22,13 +24,19 @@ namespace FSBeheer.ViewModel
 
         public RelayCommand<InspectorVM> RemoveInspectorCommand { get; set; }
 
+        public RelayCommand SaveChangesCommand { get; set; }
+
+        public RelayCommand<Window> DiscardChangesCommand { get; set; }
+
         private InspectorVM _selectedInspector { get; set; }
 
         public AvailableInspectorViewModel()
         {
             Init();
-            SetInspectorCommand = new RelayCommand<InspectorVM>(DummyMethod);
+            SetInspectorCommand = new RelayCommand<InspectorVM>(AddInspector);
             RemoveInspectorCommand = new RelayCommand<InspectorVM>(DummyMethod);
+            SaveChangesCommand = new RelayCommand(SaveChanges);
+            DiscardChangesCommand = new RelayCommand<Window>(Discard);
         }
 
         public InspectorVM SelectedInspector
@@ -48,9 +56,37 @@ namespace FSBeheer.ViewModel
             RaisePropertyChanged(nameof(AvailableInspectors));
         }
 
+        private void AddInspector(InspectorVM commandParameter)
+        {
+
+        }
+
         private void DummyMethod(InspectorVM commandParameter)
         {
             // iets
+        }
+
+        private void SaveChanges()
+        {
+            MessageBoxResult result = MessageBox.Show("Save changes?", "Confirm action", MessageBoxButton.OKCancel);
+            if (result == MessageBoxResult.OK)
+            {
+                // CustomFSContext.AvailabilityCrud.GetAvailabilities().Add(Inspector);
+                CustomFSContext.SaveChanges();
+
+                Messenger.Default.Send(true, "UpdateAvailableList"); 
+            }
+        }
+
+        private void Discard(Window window)
+        {
+            MessageBoxResult result = MessageBox.Show("Close without saving?", "Confirm discard", MessageBoxButton.OKCancel);
+            if (result == MessageBoxResult.OK)
+            {
+                CustomFSContext.Dispose();
+                // Inspector = null;
+                window.Close();
+            }
         }
     }
 }
