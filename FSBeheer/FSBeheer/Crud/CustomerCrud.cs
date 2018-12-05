56742de58fb4ca5e412ba761816529c1ec9336f6
Phi <vpnguyen@avans.nl>
2018-12-05 22:29:49 +0100

    Possible way to solve nullpointer in CustomerCrud example

--- FSBeheer/FSBeheer/Crud/CustomerCrud.cs
+++ FSBeheer/FSBeheer/Crud/CustomerCrud.cs
@@ -72,10 +72,10 @@ namespace FSBeheer.Crud
                 .Where(i => i.IsDeleted == false)
                 .Where(i =>
                 i.Id.ToString().ToLower().Contains(must_contain) ||
-                i.Name.ToLower().Contains(must_contain) ||
-                i.Address.ToLower().Contains(must_contain) ||
-                i.City.ToLower().Contains(must_contain) ||
-                i.StartingDate.ToString().ToLower().Contains(must_contain)
+                i.Name != null && i.Name.ToLower().Contains(must_contain) ||
+                i.Address != null && i.Address.ToLower().Contains(must_contain) ||
+                i.City != null && i.City.ToLower().Contains(must_contain) ||
+                i.StartingDate != null && i.StartingDate.ToString().ToLower().Contains(must_contain)
                 ).Distinct()
                 .Select(i => new CustomerVM(i));
             var _customers = new ObservableCollection<CustomerVM>(customers);
