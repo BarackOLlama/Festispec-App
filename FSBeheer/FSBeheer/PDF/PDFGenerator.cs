ff24dd47123f4bf720b812f1162920480c7a3df7
Phi <vpnguyen@avans.nl>
2019-01-14 15:33:38 +0100

    push Generator

--- FSBeheer/FSBeheer/PDF/PDFGenerator.cs
+++ FSBeheer/FSBeheer/PDF/PDFGenerator.cs
@@ -2,6 +2,7 @@
 using PdfSharp.Drawing;
 using PdfSharp.Drawing.Layout;
 using PdfSharp.Pdf;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
@@ -19,12 +20,15 @@ namespace FSBeheer.ViewModel
         private string _advice;
 
         private ObservableCollection<QuestionVM> QuestionsList;
+        private XFont font;
 
         public PDFGenerator(string Filename, string Title, string Description, string Advice, ObservableCollection<QuestionVM> Questions)
         {
             // Create a new PDF document
             document = new PdfDocument();
             document.Info.Title = "Created by Phi";
+
+            font3 = new XFont("Calibri", 20, XFontStyle.BoldItalic);
 
             _fileName = Filename;
             _title = Title;
@@ -33,15 +37,100 @@ namespace FSBeheer.ViewModel
             QuestionsList = Questions;
             foreach (var question in QuestionsList)
             {
-                System.Console.WriteLine(question.Type);
+                Console.WriteLine(question.Type);
             }
         }
 
         private void MakeFrontPage()
         {
             PdfPage page1 = document.AddPage();
-            XGraphics gfx1 = XGraphics.FromPdfPage(page1);
-            DrawTitle(page1, gfx1, _title);
+            XGraphics gfx = XGraphics.FromPdfPage(page1);
+            XRect rect;
+            XPen pen;
+            double x = 50, y = 100;
+            XFont fontH1 = new XFont("Calibri", 18, XFontStyle.Bold);
+            XFont font = new XFont("Calibri", 12);
+            XFont fontItalic = new XFont("Calibri", 12, XFontStyle.BoldItalic);
+            double ls = font.GetHeight(gfx);
+
+            DrawTitle(page1, gfx, _title);
+            gfx.DrawString(DateTime.Now.ToShortDateString(), font, XBrushes.DarkSalmon,
+              new XRect(0, 0, (page1.Width / 2), (page1.Height / 2)),
+              XStringFormats.Center);
+
+
+            // Draw some text
+            gfx.DrawString("Create PDF on the fly with PDFsharp",
+                fontH1, XBrushes.Black, x, x);
+            gfx.DrawString("With PDFsharp you can use the same code to draw graphic, " +
+                "text and images on different targets.", font, XBrushes.Black, x, y);
+            y += ls;
+            gfx.DrawString("The object used for drawing is the XGraphics object.",
+                font, XBrushes.Black, x, y);
+            y += 2 * ls;
+
+            // Draw an arc
+            pen = new XPen(XColors.Red, 4);
+            pen.DashStyle = XDashStyle.Dash;
+            gfx.DrawArc(pen, x + 20, y, 100, 60, 150, 120);
+
+            // Draw a star
+            XGraphicsState gs = gfx.Save();
+            gfx.TranslateTransform(x + 140, y + 30);
+            for (int idx = 0; idx < 360; idx += 10)
+            {
+                gfx.RotateTransform(10);
+                gfx.DrawLine(XPens.DarkGreen, 0, 0, 30, 0);
+            }
+            gfx.Restore(gs);
+
+            // Draw a rounded rectangle
+            rect = new XRect(x + 230, y, 100, 60);
+            pen = new XPen(XColors.DarkBlue, 2.5);
+            XColor color1 = XColors.DarkSeaGreen;
+            XColor color2 = XColors.Red;
+            XLinearGradientBrush lbrush = new XLinearGradientBrush(rect, color1, color2,
+              XLinearGradientMode.Vertical);
+            gfx.DrawRoundedRectangle(pen, lbrush, rect, new XSize(10, 10));
+
+            // Draw a pie
+            pen = new XPen(XColors.DarkOrange, 1.5);
+            pen.DashStyle = XDashStyle.Dot;
+            gfx.DrawPie(pen, XBrushes.Blue, x + 360, y, 100, 60, -130, 135);
+
+            // Draw some more text
+            y += 60 + 2 * ls;
+            gfx.DrawString("With XGraphics you can draw on a PDF page as well as " +
+                "on any System.Drawing.Graphics object.", font, XBrushes.Black, x, y);
+            y += ls * 1.1;
+            gfx.DrawString("Use the same code to", font, XBrushes.Black, x, y);
+            x += 10;
+            y += ls * 1.1;
+            gfx.DrawString("• draw on a newly created PDF page", font, XBrushes.Black, x, y);
+            y += ls;
+            gfx.DrawString("• draw above or beneath of the content of an existing PDF page",
+                font, XBrushes.Black, x, y);
+            y += ls;
+            gfx.DrawString("• draw in a window", font, XBrushes.Black, x, y);
+            y += ls;
+            gfx.DrawString("• draw on a printer", font, XBrushes.Black, x, y);
+            y += ls;
+            gfx.DrawString("• draw in a bitmap image", font, XBrushes.Black, x, y);
+            x -= 10;
+            y += ls * 1.1;
+            gfx.DrawString("You can also import an existing PDF page and use it like " +
+                "an image, e.g. draw it on another PDF page.", font, XBrushes.Black, x, y);
+            y += ls * 1.1 * 2;
+            gfx.DrawString("Imported PDF pages are neither drawn nor printed; create a " +
+                "PDF file to see or print them!", fontItalic, XBrushes.Firebrick, x, y);
+            y += ls * 1.1;
+            gfx.DrawString("Below this text is a PDF form that will be visible when " +
+                "viewed or printed with a PDF viewer.", fontItalic, XBrushes.Firebrick, x, y);
+            y += ls * 1.1;
+            XGraphicsState state = gfx.Save();
+            XRect rcImage = new XRect(100, y, 100, 100 * Math.Sqrt(2));
+            gfx.DrawRectangle(XBrushes.Snow, rcImage);
+            gfx.Restore(state);
         }
 
         private void SavePDF()
@@ -59,6 +148,8 @@ namespace FSBeheer.ViewModel
 
             SavePDF();
         }
+
+
 
         public void CreateStandardPDF()
         {
