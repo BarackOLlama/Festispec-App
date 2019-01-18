580146a25064f10f2b8a98388e54dfd45c777acd
gharamorth <mappelma@avans.nl>
2019-01-19 00:06:20 +0100

    The unfuckening of the CreateEditQuestionnaireViewModel (Some changes in affiliate classes without testing broke the build. Not my changes).

--- FSBeheer/FSBeheer/ViewModel/CreateEditQuestionnaireViewModel.cs
+++ FSBeheer/FSBeheer/ViewModel/CreateEditQuestionnaireViewModel.cs
@@ -210,7 +210,7 @@ namespace FSBeheer.ViewModel
                     {
                         SetQuestionnaireFromTemplate();
                     }
-                    _questionnaire.InspectionId = SelectedInspection.Id;
+                    _questionnaire.Inspection = Inspections[SelectedIndex];
                     _context.Questionnaires.Add(Questionnaire.ToModel());
                     _context.SaveChanges();
                     Messenger.Default.Send(true, "UpdateQuestionnaires");
